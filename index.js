7bd62f204001cf781fa51d4e02e99a2e8596453a
Mike <szekleym@gmail.com>
2018-10-30 10:23:52 -0600

    added new functions for practice

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 // write a function declaration called squareMe that takes in a parameter
 // and return that parameter squared
 
-function squareMe(){}
+function squareMe(num){
+  return num * num
+}
 
 // write an anonymous function literal with the variable name cubeMe that
 // takes in a parameter and returns that parameter cubed
 
-let cubeMe = () => {}
+let cubeMe = (num) => {
+  return num * num * num
+}
 
 // write a function declaration called addTwoNumbers that takes in two parameters
 // and returns the sum of those parameters
 
-function addTwoNumbers(){}
+function addTwoNumbers(num1, num2){
+  return num1 + num2
+}
 
 // write a function declaration called addManyNumbers that takes in any number of parameters
 // and returns the sum of the input parameters
 
-function addManyNumbers(){}
+function addManyNumbers(){
+  var sum = 0
+  for (var i = 0; i < arguments.length; i++){
+    sum += arguments[i]
+  }
+  return sum
+}
 
 // MUTIPLE CHOICE: INPUT THE LETTER OF THE CORRECT ANSWER INTO THE hoisting FUNCTION BELOW:
 // Which one of these functions can you call above its definition in your code?
@@ -37,7 +49,7 @@ function addManyNumbers(){}
 
 
 function hoisting () {
-  return ("YOUR ANSWER HERE").toLowerCase();
+  return ("a").toLowerCase();
 }
 
 module.exports = {
