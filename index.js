6a6a77d265d6ecb8230c45aa32fee178ab53969a
Aimee Zawacki <aimee@tna.digital>
2018-10-30 08:42:31 -0600

    tests are set up with function names ready to go

---
+++ index.js
@@ -0,0 +1,50 @@
+// write a function declaration called squareMe that takes in a parameter
+// and return that parameter squared
+
+function squareMe(){}
+
+// write an anonymous function literal with the variable name cubeMe that
+// takes in a parameter and returns that parameter cubed
+
+let cubeMe = () => {}
+
+// write a function declaration called addTwoNumbers that takes in two parameters
+// and returns the sum of those parameters
+
+function addTwoNumbers(){}
+
+// write a function declaration called addManyNumbers that takes in any number of parameters
+// and returns the sum of the input parameters
+
+function addManyNumbers(){}
+
+// MUTIPLE CHOICE: INPUT THE LETTER OF THE CORRECT ANSWER INTO THE hoisting FUNCTION BELOW:
+// Which one of these functions can you call above its definition in your code?
+/*
+    a) function iSeeALittleSilhouettoOfAMan() {
+          return "Scaramouch, scaramouch will you do the fandango"
+      }
+    b) var ohHi = function () {
+      return "Mark"
+    }
+    c) funct allYour (base) {
+      return "are belong to us"
+    }
+    d) def trogdor () {
+      return "the Burninator"
+    }
+*/
+
+
+function hoisting () {
+  return ("YOUR ANSWER HERE").toLowerCase();
+}
+
+module.exports = {
+  attendance: "GIMMIE A WORD",
+  squareMe:squareMe,
+  cubeMe:cubeMe,
+  addTwoNumbers:addTwoNumbers,
+  addManyNumbers:addManyNumbers,
+  hoisting:hoisting
+};
